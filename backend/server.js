61382fca5ebeb0a684407199f9bf6225fd9a0b0e
Sahil2426 <pawarsahil2430@gmail.com>
2026-05-26 00:37:38 +0530

    feat: setup express server with app configuration

---
+++ backend/server.js
@@ -0,0 +1,8 @@
+import dotenv from "dotenv"
+dotenv.config()
+
+import app from "./src/app.js"
+
+app.listen(process.env.PORT,()=>{
+    console.log(`server is running on port ${process.env.PORT}`)
+})
