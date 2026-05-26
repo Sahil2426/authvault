40128bfc77d6fdbaf91344bfbd4be7b1d6a6c176
Sahil2426 <pawarsahil2430@gmail.com>
2026-05-26 15:37:52 +0530

    feat: add zod validation schemas for auth

--- backend/src/utils/apiResponse.js
+++ backend/src/utils/apiResponse.js
@@ -1,7 +1,7 @@
 class ApiResponse {
   constructor(statusCode, message, data = null) {
     this.statusCode = statusCode;
-    this.success = statusCode < 400 
+    this.success = statusCode < 400;
     this.message = message;
     this.data = data;
   }
---
+++ backend/src/validations/auth.validation.js
@@ -0,0 +1,46 @@
+import { z } from "zod"
+
+const registerSchema = z.object({
+    username: z.string()
+    .min(3,"username must be atleast 3 characters")
+    .max(20,"username should not be longer than 20 characters")
+    .regex(/^[a-zA-Z0-9_]+$/,"Username can only contain letters, numbers and underscores"),
+
+    password: z.string()
+    .min(8,"password must be atleast 8 characters")
+    .max(50,"password should not be longer than 50 characters")
+    .regex(/^(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])/,"Password must contain at least one uppercase letter, one number and one special character"),
+
+    fullname: z.string()
+    .min(2,"fullname must be atleast 2 characters")
+    .max(50,"fullname should not be longer than 50 characters"),
+
+    email: z.string()
+    .email("Invalid email format"),
+})
+
+const loginSchema = z.object({
+    email: z.string()
+    .email("Invalid email format"),
+
+     password: z.string()
+    .min(8,"password must be atleast 8 characters")
+    .max(50,"password should not be longer than 50 characters")
+    .regex(/^(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])/,"Password must contain at least one uppercase letter, one number and one special character"),
+
+})
+
+const forgotPasswordSchema = z.object({
+    email: z.string()
+    .email("Invalid email format")
+})
+
+const resetPasswordSchema = z.object({
+    password: z.string()
+    .min(8,"password must be atleast 8 characters")
+    .max(50,"password should not be longer than 50 characters")
+    .regex(/^(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])/,"Password must contain at least one uppercase letter, one number and one special character"),
+
+})
+
+export { registerSchema, loginSchema, forgotPasswordSchema, resetPasswordSchema }
