f9f5a97fc6dc15dce5837c9a549d255b0e4f4159
Sahil2426 <pawarsahil2430@gmail.com>
2026-05-26 10:50:08 +0530

    feat: setup mongodb connection with mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,17 @@
 import dotenv from "dotenv"
 dotenv.config()
 
+import connectDB from "./src/config/db.js"
+
 import app from "./src/app.js"
+
+async function startServer(){
+
+await connectDB();
 
 app.listen(process.env.PORT,()=>{
     console.log(`server is running on port ${process.env.PORT}`)
 })
+}
+
+startServer()
