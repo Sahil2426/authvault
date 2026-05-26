316382d7610f6bea5b8c650b907adc280901c579
Sahil2426 <pawarsahil2430@gmail.com>
2026-05-26 14:18:18 +0530

    feat: add token model for refresh token management

---
+++ backend/src/models/Token.model.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const TokenSchema= mongoose.Schema({
+    userId:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    token:{
+        type: String,
+        required: true
+    },
+    expiresAt:{
+        type: Date,
+        required: true
+    }
+},{
+    timestamps: true
+})
+
+const TokenModel= mongoose.model("Token",TokenSchema)
+
+export default TokenModel
